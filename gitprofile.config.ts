01a94f84613f1aa914fa2bee61b292584f14a4f4
MikeyReal <mikey@boostercraft.net>
2024-05-14 17:00:57 -0400

    Update gitprofile.config.ts

--- gitprofile.config.ts
+++ gitprofile.config.ts
@@ -5,11 +5,11 @@ const CONFIG = {
     username: 'MikeyReal', // Your GitHub org/user name. (This is the only required config)
   },
   /**
-   * If you are deploying tod https://<USERNAME>.github.io/, for example your repository is at https://github.com/arifszn/arifszn.github.io, set base to '/'.
+   * If you are deploying to https://<USERNAME>.github.io/, for example your repository is at https://github.com/arifszn/arifszn.github.io, set base to '/'.
    * If you are deploying to https://<USERNAME>.github.io/<REPO_NAME>/,
    * for example your repository is at https://github.com/arifszn/portfolio, then set base to '/portfolio/'.
    */
-  base: '/Portfollo/',
+  base: '/gitprofile/',
   projects: {
     github: {
       display: true, // Display GitHub projects?
@@ -23,26 +23,6 @@ const CONFIG = {
           projects: [], // These projects will not be displayed. example: ['arifszn/my-project1', 'arifszn/my-project2']
         },
       },
-      projects: [
-        {
-          title: 'Project Name',
-          description:
-            'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nunc ut.',
-          imageUrl:
-            'https://img.freepik.com/free-vector/illustration-gallery-icon_53876-27002.jpg',
-          link: 'https://example.com',
-        },
-        {
-          title: 'Project Name',
-          description:
-            'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nunc ut.',
-          imageUrl:
-            'https://img.freepik.com/free-vector/illustration-gallery-icon_53876-27002.jpg',
-          link: 'https://example.com',
-        },
-      ],
-    },
-  },
       manual: {
         // Properties for manually specifying projects
         projects: ['arifszn/gitprofile', 'arifszn/pandora'], // List of repository names to display. example: ['arifszn/my-project1', 'arifszn/my-project2']
@@ -52,12 +32,12 @@ const CONFIG = {
       header: 'My Projects',
       // To hide the `External Projects` section, keep it empty.
   seo: {
-    title: 'Mikey's Portfolio',
+    title: 'Portfolio of Mikey',
     description: '',
     imageURL: '',
   },
   social: {
-    linkedin: 'MikeyDevelopment',
+    linkedin: '',
     twitter: '',
     mastodon: '',
     researchGate: '',
@@ -82,56 +62,80 @@ const CONFIG = {
     fileUrl:
       'https://www.w3.org/WAI/ER/tests/xhtml/testfiles/resources/pdf/dummy.pdf', // Empty fileUrl will hide the `Download Resume` button.
   },
-skills: [
-    'Java',
-    'Python',
+  skills: [
+    'PHP',
+    'Laravel',
     'JavaScript',
     'React.js',
     'Node.js',
+    'Nest.js',
     'MySQL',
-    'SpigotMC',
+    'PostgreSQL',
     'Git',
     'Docker',
+    'PHPUnit',
     'CSS',
+    'Antd',
+    'Tailwind',
   ],
   experiences: [
     {
-      company: 'Modernpvp',
-      position: 'Developer/Owner',
-      from: 'September 2023',
-      to: 'December 2024',
-      companyLink: 'https://discord.gg/modernpvp',
+      company: 'Company Name',
+      position: 'Position',
+      from: 'September 2021',
+      to: 'Present',
+      companyLink: 'https://example.com',
     },
     {
-      company: 'Tiertest',
-      position: 'Developerr',
-      from: 'September 2023',
-      to: 'December 2024',
-      companyLink: 'https://discord.gg/tiertest',
+      company: 'Company Name',
+      position: 'Position',
+      from: 'July 2019',
+      to: 'August 2021',
+      companyLink: 'https://example.com',
     },
+  ],
+  certifications: [
     {
-      company: 'You?',
-      position: 'Developer',
-      from: 'lets find out',
-      to: 'lets find out',
-      companyLink: '',
+      name: 'Lorem ipsum',
+      body: 'Lorem ipsum dolor sit amet',
+      year: 'March 2022',
+      link: 'https://example.com',
     },
   ],
   educations: [
     {
-      institution: 'Youtube.com',
-      degree: 'Java Development',
-      from: '2019',
-      to: '2020',
+      institution: 'Institution Name',
+      degree: 'Degree',
+      from: '2015',
+      to: '2019',
     },
     {
-      institution: 'Kody Simpson ',
-      degree: 'Plugin Development',
-      from: '2020',
-      to: '2024',
+      institution: 'Institution Name',
+      degree: 'Degree',
+      from: '2012',
+      to: '2014',
     },
   ],
-
+  publications: [
+    {
+      title: 'Publication Title',
+      conferenceName: '',
+      journalName: 'Journal Name',
+      authors: 'John Doe, Jane Smith',
+      link: 'https://example.com',
+      description:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+    },
+    {
+      title: 'Publication Title',
+      conferenceName: 'Conference Name',
+      journalName: '',
+      authors: 'John Doe, Jane Smith',
+      link: 'https://example.com',
+      description:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+    },
+  ],
   // Display articles from your medium or dev account. (Optional)
   blog: {
     source: 'dev', // medium | dev
@@ -147,7 +151,7 @@ skills: [
     snippetVersion: 6,
   },
   themeConfig: {
-    defaultTheme: 'night',
+    defaultTheme: 'lofi',
 
     // Hides the switch in the navbar
     // Useful if you want to support a single color mode
